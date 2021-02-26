29b143d088a3011827ce77a7517804600690226d
Marin Pavlinov Marinov <Marin.Marinov38@myhunter.cuny.edu>
2021-02-26 03:29:36 -0500

    all users working

--- slt-app/api/routes/users.py
+++ slt-app/api/routes/users.py
@@ -1,9 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+import json
 from config.keys import mongo
 
 users = Blueprint('users', __name__)
 
 @users.route('/all', methods = ['GET'])
 def get_all_users():
-    users = mongo.db.users.find()
-    return users, 200
+    all_users = mongo.db.users.find()
+    return json.dumps([learner for learner in all_users],indent=4, default=str), 200
