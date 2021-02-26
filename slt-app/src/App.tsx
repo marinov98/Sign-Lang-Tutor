898d131b902efe43f8679b68012141397dcff0e2
abidh0601 <abidh0601@gmail.com>
2021-02-25 21:01:20 -0500

    Implemented react-router

--- slt-app/src/App.tsx
+++ slt-app/src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route, Redirect, BrowserRouter} from 'react-router-dom';
 import './App.css';
+import Welcome from './components/Welcome/Welcome';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" component={Welcome} />
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/register" component={Register}/>
+    </BrowserRouter>
   );
 }
 
