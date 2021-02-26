9bc9a57d79326cb65b13d37fd5db10908a9d272f
Marin Pavlinov Marinov <Marin.Marinov38@myhunter.cuny.edu>
2021-02-25 23:46:27 -0500

    proper endpoints

--- slt-app/api/api.py
+++ slt-app/api/api.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from dotenv import load_dotenv
 
 # load environment
@@ -13,6 +14,9 @@ def create_app():
 
     # initialize flask app creation
     app = Flask(__name__)
+
+    if os.getenv("FLASK_ENV") == 'development':
+        CORS(app)
 
     initialize_db(app, db_url, db_name, mongo)
     routes.init_app(app)
--- slt-app/api/routes/__init__.py
+++ slt-app/api/routes/__init__.py
@@ -5,7 +5,7 @@ from .users import users
 
 def init_app(app):
     with app.app_context():
-        app.register_blueprint(auth, url_prefix="/auth")
-        app.register_blueprint(token, url_prefix="/token")
-        app.register_blueprint(analysis, url_prefix="/analysis")
-        app.register_blueprint(users, url_prefix="/users")
+        app.register_blueprint(auth, url_prefix="/api/auth")
+        app.register_blueprint(token, url_prefix="/api/token")
+        app.register_blueprint(analysis, url_prefix="/api/analysis")
+        app.register_blueprint(users, url_prefix="/api/users")
