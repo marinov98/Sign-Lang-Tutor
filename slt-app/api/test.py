def9331fb94d3e09cdd3eb05f2d9c3f8bdda8ae7
Marin Pavlinov Marinov <Marin.Marinov38@myhunter.cuny.edu>
2021-03-02 20:21:10 -0500

    skeleton for testins setup

---
+++ slt-app/api/test.py
@@ -0,0 +1,16 @@
+from app import app
+import unittest
+
+class SLTTesting(unittest.TestCase):
+    def test_register(self):
+        pass
+
+    def test_login(self):
+        pass
+
+    def test_users(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
