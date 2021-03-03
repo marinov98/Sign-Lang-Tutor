6839a5df3bcbd2134284b0499d868b09a8c39f5e
Marin Pavlinov Marinov <Marin.Marinov38@myhunter.cuny.edu>
2021-03-02 19:39:42 -0500

    removed duplicate routes

--- slt-app/api/routes/auth.py
+++ slt-app/api/routes/auth.py
@@ -11,14 +11,14 @@ auth = Blueprint('auth', __name__)
 @auth.route('/register', methods = ['POST'])
 def register_user():
     if not request.data:
-        return jsonify({'message': 'no data found in request'}), 409
+        return jsonify({'message': 'No data found in request!'}), 409
 
     users = mongo.db.users
     email = request.json.get("email")
     user = users.find_one({'email': email})
 
     if user:
-        return jsonify({'message': "User with this email already exists"}), 409
+        return jsonify({'message': "User with this email already exists!"}), 409
 
     first_name = request.json.get("firstName")
     last_name = request.json.get("lastName")
@@ -37,14 +37,14 @@ def register_user():
         'progress' : 'progressed'
     })
 
-    return jsonify({'message': 'User successfully created'}), 201
+    return jsonify({'message': 'User successfully created!'}), 201
 
 
 # /api/auth/login
 @auth.route('/login', methods = ['POST'])
 def login_user():
     if not request.data:
-        return jsonify({'message': 'no data found in request'}), 409
+        return jsonify({'message': 'No data found in request!'}), 409
 
     users = mongo.db.users
     email = request.json.get("email")
@@ -52,14 +52,14 @@ def login_user():
     user = users.find_one({'email': email})
 
     if not email or not password:
-        return jsonify({'message': 'blank field(s) detected'}), 409
+        return jsonify({'message': 'Blank field(s) detected!'}), 409
 
     if not user:
-        return jsonify({'message': 'email and password do not match'}), 404
+        return jsonify({'message': 'Email and password do not match!'}), 404
 
     # check password
     if not bcrypt.check_password_hash(user['password'], password):
-        return jsonify({'message': 'email and password do not match'}), 404
+        return jsonify({'message': 'Email and password do not match!'}), 404
 
     # create jwt
     access_token = create_access_token(identity=email)
