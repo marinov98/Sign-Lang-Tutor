6758cf69d3a148fb7ad47df6cc77e7ce2022afe2
abidh0601 <abidh0601@gmail.com>
2021-03-04 20:04:28 -0500

    removed unnecessary import

--- slt-app/src/UserContext.tsx
+++ slt-app/src/UserContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import { createContext } from "react";
 
 
-export const  UserContext : any = React.createContext(null);
+export const  UserContext : any = createContext(null);
