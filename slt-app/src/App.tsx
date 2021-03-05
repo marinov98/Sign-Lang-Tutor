e44f7d0444cd0e1a2e44ddd619382bfed2980701
abidh0601 <abidh0601@gmail.com>
2021-03-04 19:28:10 -0500

    setup auth in App

--- slt-app/src/App.tsx
+++ slt-app/src/App.tsx
@@ -1,26 +1,25 @@
 import React, { useState } from 'react';
-import {Route, Redirect, BrowserRouter} from 'react-router-dom';
+import { Route, Redirect, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import NavBar from './components/NavBar/NavBar';
-import {UserContext} from './UserContext';
+import { UserContext } from './UserContext';
 
 const App = () => {
 
-  const [user, setUser] = useState<any | undefined>("hello from context");
+  const [auth, setAuth] = useState<any | undefined>(null);
+
 
   return (
     <BrowserRouter>
-    <div>
-      <NavBar/>
-      <Route exact path="/" component={Home} />
-      <UserContext.Provider value={{user, setUser}}>
-        <Route path="/login" component={Login}/>
-        <Route path="/register" component={Register}/>
+      <UserContext.Provider value={{ auth, setAuth }}>
+        <NavBar/>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
       </UserContext.Provider>
-      </div>
     </BrowserRouter>
   );
 }
