3af90b253d0034f642a31cdf3c3172e4d62b7621
Christopher He <christopher.he33@myhunter.cuny.edu>
2021-03-22 15:36:56 -0400

    run prettier on Grid

--- slt-app/src/components/Grid/Grid.tsx
+++ slt-app/src/components/Grid/Grid.tsx
@@ -4,23 +4,26 @@ import { Container, Row, Col } from 'reactstrap';
 interface GridProps {
   rowSize: number;
   items: any[];
-};
+}
 
-const Grid: React.FunctionComponent<GridProps> = (props) => {
+const Grid: React.FunctionComponent<GridProps> = props => {
   const numRows = Math.ceil(props.items.length / props.rowSize);
 
   return (
     <Container>
-      {
-        Array.from(Array(numRows).keys()).map(i => // generates array [0..numRows-1]
-          <Row>
-            {
-              props.items.slice(i * props.rowSize, (i+1) * props.rowSize).map(x => // generates items on i-th row
-                <Col> {x} </Col>)
-            } 
-          </Row>
-        )
-      }
+      {Array.from(Array(numRows).keys()).map((
+        // generates array [0..numRows-1]
+        i
+      ) => (
+        <Row>
+          {props.items.slice(i * props.rowSize, (i + 1) * props.rowSize).map((
+            // generates items on i-th row
+            x
+          ) => (
+            <Col> {x} </Col>
+          ))}
+        </Row>
+      ))}
     </Container>
   );
 };
