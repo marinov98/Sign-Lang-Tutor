d08c759d1b1af01ec99408b67e83762801181dbe
Marin Marinov <Marin.Marinov38@myhunter.cuny.edu>
2021-04-06 17:43:49 -0400

    decoding image fixes

--- slt-app/api/routes/analysis.py
+++ slt-app/api/routes/analysis.py
@@ -5,22 +5,29 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required
 from config.keys import model, device
 from PIL import Image
-from io import BytesIO
+import base64
+from io import BytesIO, StringIO
 
 analysis = Blueprint("analysis", __name__)
 
 # /api/analysis/analyze
 @analysis.route("/analyze", methods=["POST"])
-@jwt_required()
+#@jwt_required()
 def infer():
     if not request.data:
         return jsonify({"msg": "No data found in request!"}), 409
-    # something like this
+    # define classes
     classes = [chr(i + 65) for i in range(26) if i != 25 and i != 9]
-    image = BytesIO(request.json.get("img"))
     try:
+        # decode image from frontend
+        url = request.json.get("img")
+        base_str = url.replace("data:image/jpeg;base64,", "")
+        decoded_img = base64.b64decode(base_str)
+        image = Image.open(BytesIO(decoded_img))
+
+        # use ml model to predict
         img = torch.as_tensor(
-            np.expand_dims(np.assarray(Image.open(image)), 0)
+            np.expand_dims(np.asarray(image), 0)
         ).to(device)
         out = model(img)
         confidence, predicted = torch.max(out, 1)
