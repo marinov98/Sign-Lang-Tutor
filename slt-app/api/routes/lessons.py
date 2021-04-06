d39da3e3acf47376f58e0b6eddb28c232ca32d99
Marin Marinov <Marin.Marinov38@myhunter.cuny.edu>
2021-04-06 18:47:14 -0400

    new single lesson route

--- slt-app/api/routes/lessons.py
+++ slt-app/api/routes/lessons.py
@@ -25,6 +25,15 @@ def get_user_lessons(module="Alphabet"):
     )
     return json.dumps([lesson for lesson in lessons], indent=4, default=str), 200
 
+# /api/lessons/user/single/<module>
+@lessons.route("/user/single/<lessonId>", methods=["GET"])
+@jwt_required()
+def get_user_lessons(module="Alphabet"):
+    lesson = mongo.db.lessons.find(
+        {_id": ObjectId(lessonId)}
+    )
+    return json.dumps(lesson, indent=4, default=str), 200
+
 
 # /api/lessons/update/<lessonId>
 @lessons.route("/update/<lessonId>", methods=["PUT, PATCH"])
