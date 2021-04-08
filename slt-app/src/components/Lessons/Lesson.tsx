fec4bfbb86a0648ccbe55c70368e4bf8904ed722
Marin Marinov <Marin.Marinov38@myhunter.cuny.edu>
2021-04-08 18:35:17 -0400

    times 100 for proper percentage

--- slt-app/src/components/Lessons/Lesson.tsx
+++ slt-app/src/components/Lessons/Lesson.tsx
@@ -67,7 +67,7 @@ const Lesson = (props: any) => {
                 <button onClick={sendPhoto}>Send Photo</button>
                 <br />
                 { analysis ?
-                  "We predicted that's an " + analysis.pred + " with " + analysis.confidence + "% confidence . " + (analysis.pred == lesson.title ? "Nice!" : "Try again!") : "" }
+                  "We predicted that's an " + analysis.pred + " with " + 100 * analysis.confidence + "% confidence . " + (analysis.pred == lesson.title ? "Nice!" : "Try again!") : "" }
               </Col>
             ) : "" }
         </Row>
