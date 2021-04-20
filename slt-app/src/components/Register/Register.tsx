e8464ad44125cca2b895b4c5cd2601152cea20fb
abidh0601 <abidh0601@gmail.com>
2021-04-20 09:09:39 -0400

    updated login and register pages to material-ui

--- slt-app/src/components/Register/Register.tsx
+++ slt-app/src/components/Register/Register.tsx
@@ -1,6 +1,8 @@
+import { Link, Paper, TextField, Typography, Button } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import React, { useContext, useState } from 'react';
-import { useHistory } from 'react-router';
-import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
+import { useHistory, Link as RouterLink } from 'react-router-dom';
 import { registerUser, UserContext } from '../../utils/auth';
 import './Register.css';
 
@@ -55,81 +57,187 @@ const Register: React.FunctionComponent = () => {
   };
 
   return (
-    <div className="mt-5">
-      <h1 className="text-center">Register</h1>
-      <Form
-        className="register-form"
-        onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}
-      >
-        {registerError !== '' ? (
-          <p className="text-danger text-center">{registerError}</p>
-        ) : null}
+    // <div className="mt-5">
+    //   <h1 className="text-center">Register</h1>
+    //   <Form
+    //     className="register-form"
+    //     onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}
+    //   >
+    //     {registerError !== '' ? (
+    //       <p className="text-danger text-center">{registerError}</p>
+    //     ) : null}
 
-        <FormGroup>
-          <Label for="FirstName">First Name (Optional)</Label>
-          <Input
-            value={firstName}
-            type="text"
-            placeholder="First Name"
-            onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
-              changeFirstName(text.target.value)
-            }
-          />
-        </FormGroup>
+    //     <FormGroup>
+    //       <Label for="FirstName">First Name (Optional)</Label>
+    //       <Input
+    //         value={firstName}
+    //         type="text"
+    //         placeholder="First Name"
+    //         onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
+    //           changeFirstName(text.target.value)
+    //         }
+    //       />
+    //     </FormGroup>
 
-        <FormGroup>
-          <Label for="LastName">Last Name (Optional)</Label>
-          <Input
-            value={lastName}
-            type="text"
-            placeholder="Last Name"
-            onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
-              changeLastName(text.target.value)
-            }
-          />
-        </FormGroup>
+    //     <FormGroup>
+    //       <Label for="LastName">Last Name (Optional)</Label>
+    //       <Input
+    //         value={lastName}
+    //         type="text"
+    //         placeholder="Last Name"
+    //         onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
+    //           changeLastName(text.target.value)
+    //         }
+    //       />
+    //     </FormGroup>
 
-        <FormGroup>
-          <Label for="email">Email</Label>
-          <Input
-            value={email}
-            type="email"
-            placeholder="Email"
-            onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
-              changeEmail(text.target.value)
-            }
-            required={true}
-          />
-        </FormGroup>
+    //     <FormGroup>
+    //       <Label for="email">Email</Label>
+    //       <Input
+    //         value={email}
+    //         type="email"
+    //         placeholder="Email"
+    //         onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
+    //           changeEmail(text.target.value)
+    //         }
+    //         required={true}
+    //       />
+    //     </FormGroup>
 
-        <FormGroup>
-          <Label for="password">Password</Label>
-          <Input
-            value={password}
-            type="password"
-            placeholder="Password"
-            onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
-              changePassword(text.target.value)
-            }
-            required={true}
-          />
-        </FormGroup>
+    //     <FormGroup>
+    //       <Label for="password">Password</Label>
+    //       <Input
+    //         value={password}
+    //         type="password"
+    //         placeholder="Password"
+    //         onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
+    //           changePassword(text.target.value)
+    //         }
+    //         required={true}
+    //       />
+    //     </FormGroup>
 
-        <FormGroup>
-          <Label for="confirmPassword">Confirm Password</Label>
-          <Input
-            value={confirmPassword}
-            type="password"
-            placeholder="Confirm Password"
-            onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
-              changeConfirmPassword(text.target.value)
-            }
-          />
-        </FormGroup>
+    //     <FormGroup>
+    //       <Label for="confirmPassword">Confirm Password</Label>
+    //       <Input
+    //         value={confirmPassword}
+    //         type="password"
+    //         placeholder="Confirm Password"
+    //         onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
+    //           changeConfirmPassword(text.target.value)
+    //         }
+    //       />
+    //     </FormGroup>
 
-        <Button className="btn-block btn-dark btn-lg">Register</Button>
-      </Form>
-    </div>
+    //     <Button className="btn-block btn-dark btn-lg">Register</Button>
+    //   </Form>
+    // </div>
+
+    <Container maxWidth="xs">
+      <Paper>
+        <form onSubmit={e => handleSubmit(e)}>
+          <Grid container spacing={3} direction="row">
+            <Grid item xs={12}>
+              <Typography align="center" variant="h5">
+                Register
+              </Typography>
+            </Grid>
+
+            {registerError !== '' ? (
+              <Grid item xs={12}>
+                <Typography color="error" align="center">
+                  {registerError}
+                </Typography>
+              </Grid>
+            ) : null}
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="First Name (optional)"
+                value={firstName}
+                name="firstName"
+                size="medium"
+                variant="outlined"
+                type="text"
+                onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
+                  changeFirstName(text.target.value)
+                }
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Last Name (optional)"
+                name="lastName"
+                value={lastName}
+                type="text"
+                size="medium"
+                variant="outlined"
+                onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
+                  changeLastName(text.target.value)
+                }
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Email"
+                name="email"
+                value={email}
+                size="medium"
+                variant="outlined"
+                onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
+                  changeEmail(text.target.value)
+                }
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Password"
+                name="password"
+                value={password}
+                size="medium"
+                variant="outlined"
+                type="password"
+                onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
+                  changePassword(text.target.value)
+                }
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Confirm Password"
+                name="confirmPassword"
+                value={confirmPassword}
+                size="medium"
+                variant="outlined"
+                type="password"
+                onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
+                  changeConfirmPassword(text.target.value)
+                }
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button fullWidth variant="contained" type="submit">
+                Register
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography align="center">
+                <Link component={RouterLink} to="/login">
+                  Already have an account? Login here.
+                </Link>
+              </Typography>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
   );
 };
 
