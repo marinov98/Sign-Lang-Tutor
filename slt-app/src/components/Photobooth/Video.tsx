d864319d8e29761dc2a824a0af2f1235596b15ad
Christopher He <christopher.he33@myhunter.cuny.edu>
2021-04-20 16:08:04 -0400

    add video component

---
+++ slt-app/src/components/Photobooth/Video.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import Webcam from 'react-webcam';
+
+interface VideoProps {
+}
+
+declare var MediaRecorder: any;
+
+const Video = (props: VideoProps) => {
+  const webcamRef = React.useRef<Webcam | null>(null);
+  const mediaRecorderRef = React.useRef<MediaRecorder | null>(null);
+  const [capturing, setCapturing] = React.useState(false);
+  const [recordedChunks, setRecordedChunks] = React.useState([]);
+
+  const handleStartCaptureClick = React.useCallback(() => {
+    setCapturing(true);
+    if (webcamRef && webcamRef.current) {
+      mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+        mimeType: "video/webm"
+      });
+    }
+
+    if (mediaRecorderRef && mediaRecorderRef.current) {
+      mediaRecorderRef.current.addEventListener(
+        "dataavailable",
+        handleDataAvailable
+      );
+      mediaRecorderRef.current.start();
+    }
+  }, [webcamRef, setCapturing, mediaRecorderRef]);
+
+  const handleDataAvailable = React.useCallback(
+    ({ data }) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => prev.concat(data));
+      }
+    },
+    [setRecordedChunks]
+  );
+
+  const handleStopCaptureClick = React.useCallback(() => {
+    if (mediaRecorderRef && mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+    }
+    setCapturing(false);
+  }, [mediaRecorderRef, webcamRef, setCapturing]);
+
+  const handleDownload = React.useCallback(() => {
+    if (recordedChunks.length) {
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm"
+      });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      document.body.appendChild(a);
+      // a.style = "display: none";
+      a.href = url;
+      a.download = "react-webcam-stream-capture.webm";
+      a.click();
+      window.URL.revokeObjectURL(url);
+      setRecordedChunks([]);
+    }
+  }, [recordedChunks]);
+
+  return (
+    <>
+      <Webcam audio={false} ref={webcamRef} />
+      {capturing ? (
+        <button onClick={handleStopCaptureClick}>Stop Capture</button>
+      ) : (
+        <button onClick={handleStartCaptureClick}>Start Capture</button>
+      )}
+      {recordedChunks.length > 0 && (
+        <button onClick={handleDownload}>Download</button>
+      )}
+    </>
+  );
+};
+
+export default Video;
