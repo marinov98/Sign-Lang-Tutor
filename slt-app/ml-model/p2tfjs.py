3a0b7ef8ddf9ed9837fcc6ba1271b66f7dcbc01f
Ajani Stewart <theamjaha@gmail.com>
2021-05-06 10:36:44 -0400

    whoops

--- slt-app/ml-model/p2tfjs.py
+++
@@ -1,6 +0,0 @@
-# import pytorch_to_keras
-# load model from file
-# set model to eval mode
-# use https://github.com/nerox8664/pytorch2keras/blob/master/tests/models/mobilinet.py example to convert to keras, using names='short'
-# just follow this honesstly
-# https://github.com/nerox8664/pytorch2keras 
