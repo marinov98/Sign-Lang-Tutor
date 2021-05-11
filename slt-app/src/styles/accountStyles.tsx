4898c84b4e7d850462b6925169f82d9425997c42
abidh0601 <abidh0601@gmail.com>
2021-05-10 21:27:34 -0400

    removed unnecessary import

--- slt-app/src/styles/accountStyles.tsx
+++ slt-app/src/styles/accountStyles.tsx
@@ -1,4 +1,4 @@
-import { createMuiTheme, makeStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 import { red } from '@material-ui/core/colors';
 
 export const useStyles = makeStyles(theme => ({
